d1098870a7cc55fbe592b1cca5cb264c0d9812d7
Peter Gorgone <947820+peltronic@users.noreply.github.com>
2021-12-13 14:59:44 -0800

    [feature/#initial-database-schema] -- update users table migration to allow nullable email & password as we won't use these at least initially; add migration for [phonenumbers] table to impl many-phonenumbers-to-one-user relationship

---
+++ database/migrations/2021_12_13_204907_create_phonenumbers_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhonenumbersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('phonenumbers', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedInteger('user_id')->comment('Foreign key to [users], identifying owner of this phone number record' );
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->string('phonenumber')->comment('The phone number stored with country code, but without spaces, dashes, etc');
+            $table->string('country', 2)->nullable()->comment('Country code in format ISO 3166 ALPHA-2');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('phonenumbers');
+    }
+}
